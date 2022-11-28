52a0a957263b4375dc64480bcfb9906c04ea835d
mukeshangrakh <mangrakh@mathworks.com>
2022-11-28 17:55:23 +0530

    Fixed Tests

--- test/tAddThem.m
+++ test/tAddThem.m
@@ -3,6 +3,8 @@ classdef tAddThem < matlab.unittest.TestCase
     methods (TestClassSetup)
         function throwStartDiagnostic(testCase)
             disp(['Running the test: ' class(testCase)]);
+            oldPath = addpath('..\code');
+            testCase.addTeardown(@()path(oldPath));
         end
     end
 
